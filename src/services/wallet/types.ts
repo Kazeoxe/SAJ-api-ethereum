e9b76c7ea986a933230db80c4919d6ba0b14c1bc
Samuel Lunion <lunion.sam@gmail.com>
2025-01-14 11:10:40 +0100

    feat(wallet) linked to front

--- src/services/wallet/types.ts
+++ src/services/wallet/types.ts
@@ -1,19 +1,25 @@
 export interface IWallet {
-  id: number;
-  userId: string;
-  address: string;
-  createdAt: Date;
-}
-
-export interface IWalletResponse {
-  wallet: string;
-  id: number;
-  userId: string;
-  createdAt: Date;
-}
-
-export interface IWalletService {
-  getWallet(userId: string): Promise<IWalletResponse | null>;
-  updateWallet(userId: string, address: string): Promise<IWalletResponse>;
-  getPriceHistory(symbol?: string): Promise<Array<{date: string; price: number}>>;
-}
+    id: number;
+    userId: string;
+    address: string;
+    createdAt: Date;
+  }
+  
+  export interface IWalletResponse {
+    wallet: string;
+    id: number;
+    userId: string;
+    createdAt: Date;
+  }
+  
+  export interface PriceHistoryResponse {
+    date: string;
+    price: number;
+  }
+  
+  export interface IWalletService {
+    getWallet(userId: string): Promise<IWalletResponse | null>;
+    updateWallet(userId: string, address: string): Promise<IWalletResponse>;
+    getPriceHistory(symbol?: string): Promise<PriceHistoryResponse[]>;
+    getWalletHistory(userId: string): Promise<PriceHistoryResponse[]>;
+  }
